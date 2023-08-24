076ac77ce3e2e149139e4014e3f6e83be5eb001d
evgeniachedleeva <ev.chedly@gmail.com>
2023-08-24 14:35:00 +1000

    feat: reverse js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let isNegative = false;
+  let integerString = String(n)
+
+  if (Math.sign(n) === -1) {
+    isNegative = true;
+    integerString = integerString.replaceAll("-", "");
+  }
+
+  const reversedStringArray = String(integerString).split("").reverse("");
+  const result = reversedStringArray.toString().replaceAll(",", "");
+
+  return isNegative ? Number(`${result}`) : Number(result);
 }
